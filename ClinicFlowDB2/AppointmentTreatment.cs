0de69a8d5e2a38ee8504a178f880bafcd07f4646
Pauline <plinekaeorg@gmail.com>
2026-02-01 23:21:26 +0100

    Added appointmenttreatment class

--- ClinicFlowDB2/AppointmentTreatment.cs
+++ ClinicFlowDB2/AppointmentTreatment.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClinicFlowDB2
+﻿namespace ClinicFlowApp.Models
 {
-    internal class AppointmentTreatment
+    public class AppointmentTreatment
     {
+        public int AppointmentTreatmentID { get; set; }
+
+        public int AppointmentID { get; set; }
+        public Appointment Appointment { get; set; }
+
+        public int TreatmentID { get; set; }
+        public Treatment Treatment { get; set; }
     }
 }
