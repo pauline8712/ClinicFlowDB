3d90da02c983889356193fa03de72f527d6f2fba
Pauline <plinekaeorg@gmail.com>
2026-02-01 23:17:21 +0100

    Patient class with attributes and a interface with appointment

--- ClinicFlowDB2/Patient.cs
+++ ClinicFlowDB2/Patient.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using ClinicFlowDB2;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ClinicFlowDB2
+namespace ClinicFlowApp.Models
 {
-    internal class Patient
+    public class Patient
     {
+        public int PatientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; }
     }
 }
